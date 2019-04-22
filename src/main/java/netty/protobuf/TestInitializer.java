aadf2b50c3842a182c9842996a44ce788cc07413
fanbopeng <1197624853@qq.com>
2019-04-22 15:29:20 +0800

    protobuf

---
+++ src/main/java/netty/protobuf/TestInitializer.java
@@ -0,0 +1,28 @@
+package netty.protobuf;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.protobuf.ProtobufDecoder;
+import io.netty.handler.codec.protobuf.ProtobufEncoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
+
+/**
+ * @author: fanbopeng
+ * @Date: 2019/4/21 11:54
+ * @Description:
+ */
+public class TestInitializer  extends ChannelInitializer<SocketChannel> {
+
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast(new ProtobufVarint32FrameDecoder());
+        pipeline.addLast(new ProtobufDecoder(null));    //将PB构造出来的字节数组解码
+        pipeline.addLast(new ProtobufVarint32LengthFieldPrepender());
+        pipeline.addLast(new ProtobufEncoder());
+    }
+}
---
+++ src/main/java/netty/protobuf/TestServerHandler.java
@@ -0,0 +1,9 @@
+package netty.protobuf;
+
+/**
+ * @author: fanbopeng
+ * @Date: 2019/4/21 11:56
+ * @Description:
+ */
+public class TestServerHandler  {
+}
